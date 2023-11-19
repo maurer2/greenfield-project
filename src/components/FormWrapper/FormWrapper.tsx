297431e5e2a30ff6397d3c063b9cd5a257ae5666
Martin <mr.mmaurer@gmail.com>
2023-11-19 21:58:52 +0000

    Added proper zod schema and improved validation logic

--- src/components/FormWrapper/FormWrapper.tsx
+++ src/components/FormWrapper/FormWrapper.tsx
@@ -3,39 +3,24 @@
 import { FormEvent, ReactElement } from "react";
 import { useForm } from "@tanstack/react-form";
 import { zodValidator } from "@tanstack/zod-form-adapter";
-import { z } from "zod";
+import { ZodError } from "zod";
 
-import InputField from "../InputField/InputField";
-import SelectBox from "../SelectBox/SelectBox";
+import InputField from "@/components/InputField/InputField";
+import SelectBox from "@/components/SelectBox/SelectBox";
+
+import searchFormSchema from "@/schemas/searchForm/searchForm";
+import type { SearchFormSchema } from "@/schemas/searchForm/searchForm";
 
 import * as styles from "./FormWrapper.css";
-
-const units = ["sqm", "sqft"] as const satisfies readonly string[];
-export type Unit = (typeof units)[number];
-
-type FormValues = {
-  amount: string; // todo
-  unit: Unit;
-};
-
-export function listFormatterOr<T extends string[]>(list: T): string {
-  return new Intl.ListFormat("en-gb", {
-    style: "long",
-    type: "disjunction",
-  }).format(list);
-}
 
 const FormWrapper = (): ReactElement => {
   const form = useForm({
     defaultValues: {
-      amount: "1",
+      amount: 0,
       unit: "sqm",
-    } satisfies FormValues,
+    } satisfies SearchFormSchema,
     onSubmit: async (values) => {
-      console.log(values);
-    },
-    onSubmitInvalid: async (values) => {
-      console.log(values);
+      console.log('submitted values', values);
     },
     validator: zodValidator,
   });
@@ -52,39 +37,50 @@ const FormWrapper = (): ReactElement => {
         <div className={styles.wrapper}>
           <form.Field
             name="amount"
-            onChange={z.coerce
-              .number({
-                required_error: "Amount is required",
-                invalid_type_error: "Amount must be number-like",
-              })
-              .positive("Amount must be at least 1")}
+            defaultValue={form.options.defaultValues?.amount}
+            onChange={searchFormSchema.shape.amount}
+            // workaround to show errors when untouched form is submitted
+            // https://github.com/TanStack/form/issues/469
+            // breaks resubmitting -> fix https://github.com/TanStack/form/pull/520
+            onSubmitAsync={async (value) => {
+              try {
+                searchFormSchema.shape.amount.parse(value)
+                return null;
+              } catch (error) {
+                if (error instanceof ZodError) {
+                  return error.issues[0].message;
+                }
+                return 'Unknown Error'
+              }
+            }}
             children={(field) => (
               <InputField
                 label="Amount"
-                value={field.state.value ?? "1"}
+                // 0 needs to be allowed
+                value={(typeof field.state?.value === 'undefined') || Number.isNaN(field.state?.value)
+                  ? ''
+                  : field.state.value.toString()
+                }
                 errors={field.state.meta.touchedErrors}
                 onBlur={field.handleBlur}
                 onChange={(event: FormEvent<HTMLInputElement>) => {
-                  field.handleChange(event.currentTarget.value);
+                  // https://github.com/orgs/react-hook-form/discussions/10530
+                  field.handleChange(parseInt( event.currentTarget.value, 10));
                 }}
               />
             )}
           />
           <form.Field
             name="unit"
-            onChange={z.enum(units, {
-              // https://github.com/colinhacks/zod/issues/580#issuecomment-1425044684
-              errorMap: (issue, ctx) => ({
-                message: `Unit must be ${listFormatterOr([...units])}`,
-              }),
-            })}
+            defaultValue={form.options.defaultValues?.unit}
+            onChange={searchFormSchema.shape.unit}
             children={(field) => (
               <SelectBox
                 label="Unit"
                 value={field.state.value}
                 errors={field.state.meta.touchedErrors}
                 onBlur={field.handleBlur}
-                options={[...units]}
+                options={searchFormSchema.shape.unit.options}
                 onChange={(event: FormEvent<HTMLSelectElement>) => {
                   const { value } = event.currentTarget;
 
