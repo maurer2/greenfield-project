fbfc0edcf93b8884560150bf6c5ac172409bb6e1
Martin <mr.mmaurer@gmail.com>
2024-04-07 02:42:30 +0100

    WIP replace custom form handling with RHF
    # Conflicts:
    #	package.json
    #	src/components/FormWrapper/FormWrapper.tsx

--- src/components/FormContent/FormContent.tsx
+++ src/components/FormContent/FormContent.tsx
@@ -2,76 +2,78 @@
 
 import type { SearchFormSubmitActionResult } from '@/app/actions/handleSearchFormSubmit/handleSearchFormSubmit';
 import type { SearchFormValues } from '@/schemas/searchForm/searchForm';
-import type { FormEvent, ReactElement, Reducer } from 'react';
+import type { FormEvent, ReactElement } from 'react';
 
 import InputField from '@/components/InputField/InputField';
 import SelectBox from '@/components/SelectBox/SelectBox';
 import searchFormSchema, { units } from '@/schemas/searchForm/searchForm';
 import 'animate.css';
 import clsx from 'clsx';
-import { useReducer } from 'react';
 import { useFormStatus } from 'react-dom';
+import { useFormContext } from 'react-hook-form';
 import { deserializeError } from 'serialize-error';
 
 import SubmitButton from '../SubmitButton/SubmitButton';
 import * as styles from './FormContent.css';
 
-type FormValuesActionsMap = {
-  [K in keyof SearchFormValues]: {
-    payload: SearchFormValues[K];
-    type: `UPDATE_${Uppercase<K>}`;
-  };
-} & {
-  reset: {
-    payload: never;
-    type: 'RESET';
-  };
-};
-type FormValuesActions = FormValuesActionsMap[keyof FormValuesActionsMap];
+// type FormValuesActionsMap = {
+//   [K in keyof SearchFormValues]: {
+//     payload: SearchFormValues[K];
+//     type: `UPDATE_${Uppercase<K>}`;
+//   };
+// } & {
+//   reset: {
+//     payload: never;
+//     type: 'RESET';
+//   };
+// };
+// type FormValuesActions = FormValuesActionsMap[keyof FormValuesActionsMap];
 
 export type FormContentProps = {
   formState: SearchFormSubmitActionResult;
 };
 
-const formValuesDefaultValues: SearchFormValues = {
-  amount: 1,
-  unit: 'sqm',
-};
+// const formValuesDefaultValues: SearchFormValues = {
+//   amount: 1,
+//   unit: 'sqm',
+// };
 
-const formValuesReducer = (state: SearchFormValues, action: FormValuesActions) => {
-  switch (action.type) {
-    case 'UPDATE_AMOUNT': {
-      return {
-        ...state,
-        amount: action.payload,
-      };
-    }
-    case 'UPDATE_UNIT': {
-      return {
-        ...state,
-        unit: action.payload,
-      };
-    }
-    case 'RESET': {
-      return {
-        ...formValuesDefaultValues,
-      };
-    }
-    default: {
-      return {
-        ...state,
-      };
-    }
-  }
-};
+// const formValuesReducer = (state: SearchFormValues, action: FormValuesActions) => {
+//   switch (action.type) {
+//     case 'UPDATE_AMOUNT': {
+//       return {
+//         ...state,
+//         amount: action.payload,
+//       };
+//     }
+//     case 'UPDATE_UNIT': {
+//       return {
+//         ...state,
+//         unit: action.payload,
+//       };
+//     }
+//     case 'RESET': {
+//       return {
+//         ...formValuesDefaultValues,
+//       };
+//     }
+//     default: {
+//       return {
+//         ...state,
+//       };
+//     }
+//   }
+// };
 
 function FormContent({ formState }: FormContentProps): ReactElement {
-  const [formValues, dispatchFormValues] = useReducer<Reducer<SearchFormValues, FormValuesActions>>(
-    formValuesReducer,
-    formValuesDefaultValues,
-  );
+  // const [formValues, dispatchFormValues] = useReducer<Reducer<SearchFormValues, FormValuesActions>>(
+  //   formValuesReducer,
+  //   formValuesDefaultValues,
+  // );
+  const methods = useFormContext<SearchFormValues>();
   const { pending } = useFormStatus();
 
+  // server state
   const isError = formState?.status === 'error';
   const isFailedValidation = formState?.status === 'validation-fail';
 
@@ -86,38 +88,38 @@ function FormContent({ formState }: FormContentProps): ReactElement {
         errors={isFailedValidation ? formState.errors.fieldErrors?.amount : undefined}
         label="Amount"
         name="amount"
-        onChange={(event: FormEvent<HTMLInputElement>) => {
-          const { valueAsNumber } = event.currentTarget;
+        // onChange={(event: FormEvent<HTMLInputElement>) => {
+        //   const { valueAsNumber } = event.currentTarget;
 
-          dispatchFormValues({
-            payload: Number.isNaN(valueAsNumber) ? '' : valueAsNumber,
-            type: 'UPDATE_AMOUNT',
-          });
-        }}
-        value={formValues.amount.toString()}
+        //   dispatchFormValues({
+        //     payload: Number.isNaN(valueAsNumber) ? '' : valueAsNumber,
+        //     type: 'UPDATE_AMOUNT',
+        //   });
+        // }}
+        // value={formValues.amount.toString()}
       />
       <SelectBox
         errors={isFailedValidation ? formState.errors.fieldErrors?.unit : undefined}
         label="Unit"
         name="unit"
-        onBlur={() => {}}
-        onChange={(event: FormEvent<HTMLSelectElement>) => {
-          const { value } = event.currentTarget;
+        // onBlur={() => {}}
+        // onChange={(event: FormEvent<HTMLSelectElement>) => {
+        //   const { value } = event.currentTarget;
 
-          if (!units.includes(value)) {
-            return;
-          }
+        //   if (!units.includes(value)) {
+        //     return;
+        //   }
 
-          // value has now been narrowed to one of the const values
-          dispatchFormValues({
-            payload: value as typeof formValues.unit,
-            type: 'UPDATE_UNIT',
-          });
-        }}
+        //   // value has now been narrowed to one of the const values
+        //   dispatchFormValues({
+        //     payload: value as typeof formValues.unit,
+        //     type: 'UPDATE_UNIT',
+        //   });
+        // }}
         options={searchFormSchema.shape.unit.options}
-        value={formValues.unit}
+        // value={formValues.unit}
       />
-      <SubmitButton isDisabled={pending}>Calculate</SubmitButton>
+      <SubmitButton isDisabled={pending || !methods.formState.isValid}>Calculate</SubmitButton>
       {isError && !pending && (
         <output className={styles.output}>{deserializeError(formState.error).message}</output>
       )}
