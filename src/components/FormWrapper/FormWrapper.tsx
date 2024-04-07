21a854840264532241aaf32a07010ae9a7119e14
Martin <mr.mmaurer@gmail.com>
2024-04-07 02:42:30 +0100

    WIP replace custom form handling with RHF

--- src/components/FormWrapper/FormWrapper.tsx
+++ src/components/FormWrapper/FormWrapper.tsx
@@ -1,11 +1,10 @@
 'use client';
 
 import type { FormContentProps } from '@/components/FormContent/FormContent';
-import type { SearchFormSchema } from '@/schemas/searchForm/searchForm';
-import type { ReactElement } from 'react';
+import type { SearchFormSchema, SearchFormValues } from '@/schemas/searchForm/searchForm';
+import type { ElementType, ReactElement } from 'react';
 
 import { handleSearchFormSubmit } from '@/app/actions/handleSearchFormSubmit/handleSearchFormSubmit';
-import { DevTool } from '@hookform/devtools';
 import dynamic from 'next/dynamic';
 import { useFormState } from 'react-dom';
 import { Form, FormProvider, useForm } from 'react-hook-form';
@@ -14,24 +13,24 @@ import * as styles from './FormWrapper.css';
 
 // avoid hydration errors
 // https://github.com/react-hook-form/devtools/issues/187
-const DevTools: React.ElementType = dynamic(
+const DevTools: ElementType = dynamic(
   () => import('@hookform/devtools').then((module) => module.DevTool),
   { ssr: false },
 );
 
 type FormWrapperProps = {
-  children: (props: FormContentProps) => ReactElement; // Faac
+  children: (props: FormContentProps) => ReactElement; // FaaC
 };
 
 // wrapper required to be able to use useFormStatus in child
 function FormWrapper({ children }: FormWrapperProps): ReactElement {
   const [formState, action] = useFormState(handleSearchFormSubmit, null);
-  const methods = useForm<SearchFormSchema>({
+  const methods = useForm<SearchFormValues>({
     defaultValues: {
       amount: 1,
       unit: 'sqm',
     },
-    progressive: true,
+    progressive: true, // adds necessary hidden fields for form action
   });
 
   return (
@@ -40,10 +39,11 @@ function FormWrapper({ children }: FormWrapperProps): ReactElement {
         action={action}
         className={styles.formWrapper}
         control={methods.control}
-        // method="post"
-        onError={() => {}}
-        onSubmit={() => {}}
-        onSuccess={() => {}}
+        // @ts-expect-error union type mismatch
+        method="POST"
+        // onError={() => {}}
+        // onSubmit={() => {}}
+        // onSuccess={() => {}}
       >
         {children({ formState })}
       </Form>
