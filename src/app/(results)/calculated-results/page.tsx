99c7d1c7d02db03bccc54e544d4c8c33ee365105
Martin <mr.mmaurer@gmail.com>
2026-05-16 23:58:22 +0200

    Added missing suspense boundary around client components that use query params

--- src/app/(results)/calculated-results/page.tsx
+++ src/app/(results)/calculated-results/page.tsx
@@ -1,6 +1,7 @@
 import ComparisonBox from '@/components/ComparisonBox/ComparisonBox';
 import { loadCalculationSearchParams } from '@/lib/calculationSearchParams/calculationSearchParams';
 
+// Page is dynamically rendered according to "next build", but the next dev overlay shows it as static
 export default async function Results({ searchParams }: PageProps<'/calculated-results'>) {
   // triggers closest error.tsx if invalid
   const { amount, unit } = await loadCalculationSearchParams(searchParams, { strict: true });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
+import { Suspense } from 'react';
+
 import FormContent from '@/components/FormContent/FormContent';
 import FormWrapper from '@/components/FormWrapper/FormWrapper';
 
 export default function Home() {
   return (
-    <FormWrapper>
-      <FormContent />
-    </FormWrapper>
+    <Suspense>
+      <FormWrapper>
+        <FormContent />
+      </FormWrapper>
+    </Suspense>
   );
 }
